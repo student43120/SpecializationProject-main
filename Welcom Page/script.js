29fcb3ff468c269f52e5a4c2a634187e3e859949
student43120 <dsw43120@student.dsw.edu.pl>
2023-11-30 23:39:44 +0100

    Upgrade example welcom page for project

---
+++ Welcom Page/script.js
@@ -0,0 +1,9 @@
+document.addEventListener('DOMContentLoaded', function() {
+  const startButton = document.getElementById('startButton');
+
+
+  startButton.addEventListener('click', function() {
+
+    alert('Let\'s get started!');
+  });
+});
